5228a8e5aa5b0968dc6e0417d0eb4d9c5b5922b0
Vladimir Belkin <vladimirB2000@mail.ru>
2019-08-05 21:08:37 +0700

    Initial commit

---
+++ src/JavaRush/Solution.java
@@ -0,0 +1,18 @@
+package JavaRush;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        String number = reader.readLine();
+        int a = Integer.parseInt(number);
+        while(a>0)
+        {System.out.println(s);
+        a--;}
+
+
+
+    }
+}
